7d48d90d3a4c744e8c08b43c75913f8a5c866039
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-11 19:11:10 +0200

    created an interface for news articles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
+import { Container } from 'react-bootstrap';
+import styles from '@/styles/App.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
     </div>
   );
 }
