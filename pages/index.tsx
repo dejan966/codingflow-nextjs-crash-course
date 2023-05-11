403e53ae5cd4326b7627a9f7a6e93da4fceec4ea
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-11 19:59:27 +0200

    Breaking News page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,15 @@ import styles from "@/styles/Home.module.css";
 import { GetServerSideProps } from "next";
 import { NewsArticle, NewsResponse } from "@/models/NewsArticles";
 import NewsArticleEntry from "@/components/NewsAricleEntry";
+import NewsArticlesGrid from "@/components/NewsArticlesGrid";
+import { Alert } from "react-bootstrap";
 
 interface BreakingNewsPageProps{
   newsArticles:NewsArticle[],
 }
 
 export const getServerSideProps:GetServerSideProps<BreakingNewsPageProps> = async () =>{
+  await new Promise(r => setTimeout(r, 3000));
   const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWS_API_KEY);
   const newsResponse : NewsResponse = await response.json();
   return {
@@ -26,7 +29,11 @@ export default function BreakingNewsPage({newsArticles}:BreakingNewsPageProps) {
       </Head>
       <main>
         <h1>Breaking News</h1>
-        <NewsArticleEntry article={newsArticles[0]}/>
+        <Alert>
+          This page uses <strong>getServerSideProps</strong> to fetch data server-side on every request. 
+          This alows search engines to crawl the page content and <strong>improves SEO</strong>. 
+        </Alert>
+        <NewsArticlesGrid articles={newsArticles}/>
       </main>
     </>
   );
