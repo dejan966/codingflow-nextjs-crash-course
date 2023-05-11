91350e00cf6943162d71502b5269f248afd43f23
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-11 18:41:38 +0200

    removed some css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+       <Head>
+        <title>Nextjs News App</title>
+        <meta name="description" content="NextJs crash course by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
