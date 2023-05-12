114c23c494729123e0a6cf0c8e991dcd2ae8c352
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-12 12:57:21 +0200

    Adding search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,9 +1,30 @@
+import { NewsArticle } from "@/models/NewsArticles";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
 const SearchNewsPage = () => {
-    return ( 
-        <main>
-            <h1>Search News</h1>
-        </main>
-     );
-}
- 
+  const [searchResults, setSearchResults] = useState<NewsArticle[] | null>(
+    null
+  );
+  const [searchResultsLoading, setSearchResultsLoading] = useState(false);
+  const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] =
+    useState(false);
+
+  return (
+    <main>
+      <h1>Search News</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="search-input">
+          <Form.Label>Search query</Form.Label>
+          <Form.Control
+            name="searchQuery"
+            placeholder="e.g. politics, sports,..."
+          />
+          <Button type="submit">Search</Button>
+        </Form.Group>
+      </Form>
+    </main>
+  );
+};
+
 export default SearchNewsPage;
