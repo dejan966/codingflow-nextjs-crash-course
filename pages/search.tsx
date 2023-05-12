a9b5bbb1a4735929d8e90581a1363923de551562
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-05-12 13:07:43 +0200

    gives an alert when you seach

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,5 +1,5 @@
 import { NewsArticle } from "@/models/NewsArticles";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const SearchNewsPage = () => {
@@ -10,17 +10,33 @@ const SearchNewsPage = () => {
   const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] =
     useState(false);
 
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault(); //prevents the page from refreshing
+    const formData = new FormData(e.target as HTMLFormElement);
+    const searchQuery = formData.get("searchQuery")?.toString().trim(); //trim removes white space
+
+    if (searchQuery) {
+      alert(searchQuery);
+    }
+  }
+
   return (
     <main>
       <h1>Search News</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="search-input">
           <Form.Label>Search query</Form.Label>
           <Form.Control
             name="searchQuery"
             placeholder="e.g. politics, sports,..."
           />
-          <Button type="submit">Search</Button>
+          <Button
+            type="submit"
+            className="mb-3"
+            disabled={searchResultsLoading}
+          >
+            Search
+          </Button>
         </Form.Group>
       </Form>
     </main>
